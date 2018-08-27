16d14147b09aaf762df7849c619c2cbc9f8faa27
(ios) Konstantin Antonyuk <ka@uniphone.biz>
2018-08-27 10:21:34 +0300

    initial commit

---
+++ TimeLine/TimeLine/Sources/Authorization/Login/LoginScreen.swift
@@ -0,0 +1,18 @@
+//
+//  LoginScreen.swift
+//  TimeLine
+//
+//  Created by UT on 27.08.2018.
+//  Copyright © 2018 kostindevelop. All rights reserved.
+//
+
+import UIKit
+
+class LoginScreen: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
